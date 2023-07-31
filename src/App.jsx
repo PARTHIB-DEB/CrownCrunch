b13a5505604376748bf2032e884cf657df799d90
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-07-31 10:43:16 +0530

    navbar applied and welcome banner started

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from './components/navbar';
 
 export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
-    </>
+    <div>
+      <NavBar/>
+    </div>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+const bgimage = {
+    backgroundImage: 'url("banners/start-Banner.jpg")'
+}
+
+export default function NavBar() {
+    return (
+        <header
+            className="navbar bg-cover bg-center h-[35rem] p-[0.5em] max-h-[35rem]"
+            style={bgimage}
+        >
+            <div className="nav-items flex justify-center items-center">
+                <img
+                    src="/logo-crowncrunch.png"
+                    alt=""
+                    srcSet=""
+                    className="h-[6rem] -ml-[18rem] mr-[12rem]"
+                />
+                <div className="items flex space-x-[2.5rem] font-bold text-lg text-slate-300 ">
+                    <p className="home hover:text-white cursor-pointer">Home</p>
+                    <p className="services hover:text-white cursor-pointer">Services</p>
+                    <p className="menu hover:text-white cursor-pointer">Menu</p>
+                    <p className="about hover:text-white cursor-pointer">About</p>
+                    <p className="blog hover:text-white cursor-pointer">Blog</p>
+                    <p className="contact hover:text-white cursor-pointer">Contact</p>
+                </div>
+                <button
+                    type="button"
+                    className="ml-[10rem] -mr-[18rem] rounded-none text-black bg-white p-[4rem] py-[0.8vw] px-[1.3rem] hover:bg-green-600 hover:text-white"
+                >
+                    <p className="text-center">RESERVATION</p>
+                </button>
+            </div>
+        </header>
+    )
+}
