fe0a3c28fb9a9c0d1e5d20d7e121b8353304c50c
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-08-03 16:17:49 +0530

    Navbar and Welcome Message Complete(++Responsive)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/navbar';
+
 export default function App() {
   const [count, setCount] = useState(0)
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-const bgimage = {
-    backgroundImage: 'url("banners/start-Banner.jpg")',
-};
 
 export default function NavBar() {
     return (
@@ -43,7 +40,7 @@ export default function NavBar() {
                     srcSet=""
                     className="h-[3.5rem] w-[3.5rem] -ml-[18rem] mr-[12rem]"
                 />
-                <div className="items flex space-x-[2.5rem] font-bold text-lg text-slate-300 ">
+                <div className="items flex space-x-[2.5rem] font-bold text-lg text-slate-300">
                     <p className="home hover:text-white cursor-pointer">Home</p>
                     <p className="services hover:text-white cursor-pointer">Services</p>
                     <p className="menu hover:text-white cursor-pointer">Menu</p>
@@ -53,12 +50,68 @@ export default function NavBar() {
                 </div>
                 <button
                     type="button"
-                    className="ml-[10rem] -mr-[18rem] rounded-none text-black bg-white p-[4rem] py-[0.8vw] px-[1.3rem] hover:bg-green-600 hover:text-white"
+                    className="ml-[10rem] -mr-[18rem] rounded-none bg-white text-black p-[4rem] py-[0.8vw] px-[1.3rem] hover:bg-green-600 hover:text-white"
                 >
                     <p className="text-center">RESERVATION</p>
                 </button>
             </div>
+            {/*Welcome Description Of Site*/}
+            <div className="flex flex-col  w-auto h-[20rem] mt-[2.5rem]">
+                <p
+                    className="font-serif text-white text-7xl mb-[1vw] font-bold text-center"
+                    id="para1"
+                >
+                    DELICIOUS FOOD
+                </p>
+                <p
+                    className="font-serif text-white text-7xl mb-[1vw] font-bold text-center"
+                    id="para2"
+                >
+                    FOR ANY EVENT
+                </p>
+                <p className="font-serif text-white text-3xl text-center" id="para3">
+                    Anytime!!
+                </p>
+                <p className="font-serif text-white text-3xl text-center" id="para4">
+                    Anywhere!!
+                </p>
+                <div className="duo-btn flex justify-center space-x-4 flex-wrap items-center mt-[1rem]">
+                    <button
+                        type="button"
+                        className="rounded-none bg-green-600 text-white p-[4rem] py-[0.8vw] px-[1.3rem] hover:bg-green-500"
+                    >
+                        <p className="text-center text-base">Today's Menu</p>
+                    </button>
+                    <button
+                        type="button"
+                        className="rounded-none bg-white text-black p-[4rem] py-[0.8vw] px-[1.3rem] hover:bg-green-600 hover:text-white"
+                        id="second-btn"
+                    >
+                        <p className="text-center text-base">About Us</p>
+                    </button>
+                </div>
+            </div>
+            {/*SEPERATED FOR HAM-ITEMS*/}
+            <div className="flex justify-center w-auto h-[20rem] -mt-[20rem]">
+                {/*Items Of hamburger*/}
+                <div
+                    className="ham-items flex flex-col items-center justify-evenly h-auto w-[20rem] bg-green-600 font-bold text-lg text-slate-300 -translate-y-[50rem]"
+                    id="navItems"
+                >
+                    <p className="home hover:text-white cursor-pointer">Home</p>
+                    <p className="services hover:text-white cursor-pointer">Services</p>
+                    <p className="menu hover:text-white cursor-pointer">Menu</p>
+                    <p className="about hover:text-white cursor-pointer">About</p>
+                    <p className="blog hover:text-white cursor-pointer">Blog</p>
+                    <p className="contact hover:text-white cursor-pointer">Contact</p>
+                    <button
+                        type="button"
+                        className="rounded-none bg-white text-black p-[4rem] py-[0.8vw] px-[1.3rem] hover:bg-yellow-600 hover:text-white"
+                    >
+                        <p className="text-center">RESERVATION</p>
+                    </button>
+                </div>
+            </div>
         </header>
-
     );
 }
